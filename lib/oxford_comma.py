82e3f9a67b335e1c93f9cf87bd41e6bc9b7421a4
John DH. Park <john.dh.park@icloud.com>
2023-08-08 08:10:48 -0700

    Completed Oxford Comma Lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(fruits):
+
+    if len(fruits) == 1:
+        return fruits[0]
+    elif len(fruits) == 2:
+        return " and ".join(fruits)
+    elif len(fruits) == 3:
+        return fruits[0] + ", " + fruits[1] + ", and " + fruits[2]
+    else:
+        return ", ".join(fruits[0:-1]) + ", and " + fruits[-1]
